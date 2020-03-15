5402716cc7cc5932c2ae89696d21b8baab85ab07
unknown <sergishramchenko@gmail.com>
2020-03-15 21:03:16 +0300

    styling homePage, create folder strucuture

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import HomePage from './pages/HomePage';
+import HomePage from './pages/home-page/HomePage';
 import './App.css';
 
 function App() {
---
+++ src/pages/home-page/HomePage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import Directory from '../../components/directory/Directory';
+
+import './homePage.scss';
+
+export default () => <Directory />;
