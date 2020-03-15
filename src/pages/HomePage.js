65ab6d6dfb5be1a0a84607fbd19195622f1bb38c
unknown <sergishramchenko@gmail.com>
2020-03-15 20:07:46 +0300

    create project and add homePage structure

---
+++ src/pages/HomePage.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import './homePage.scss';
+
+export default () => (
+  <div className='homepage'>
+    <div className='directory-menu'>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>LAPTOPS</h1>
+          <span className='subtitle'>BUY NOW</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>MONITORS</h1>
+          <span className='subtitle'>BUY NOW</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>KEYBOARDS</h1>
+          <span className='subtitle'>BUY NOW</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>MOUSES</h1>
+          <span className='subtitle'>BUY NOW</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>HEADPHONES</h1>
+          <span className='subtitle'>BUY NOW</span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
